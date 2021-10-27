f1783c9878fb2cad9f41d1fd13b3a564ae793a0d
Bermu <lucas.bermudez135@gmail.com>
2021-10-27 19:41:37 -0300

    Desafio 8

--- miapp/src/App.js
+++ miapp/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state={
+      newTask:"Aca va a ir el estado cuando lo cambiemos",
+      numero:0
+    }
+    this.cont = 1
+    this.handleTaskChange=this.handleTaskChange.bind(this)
+  }
+  handleTaskChange(evt){
+    this.cont++
+    this.setState({newTask: evt.target.value,numero:this.cont})
+  }
+  render(){
+    return(
+      <div className="container bg-dark">
+        <div className="row ">
+          <h1 className="text-center text-white">Hola Mundo!</h1>
+          <span className="text-white">{this.state.numero}</span>
+          <div className="d-flex justify-content-center">
+            <input onChange={this.handleTaskChange} type="text" className = "w-25"></input>
+          </div>
+          
+          <h2 className="text-white text-center">{this.state.newTask}</h2>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
